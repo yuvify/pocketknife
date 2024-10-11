6dc11c79d5156612e3ec56c725582619d007d61e
yuvify <yuvi396@outlook.com>
2024-10-11 14:16:07 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const fastify = require("fastify")({ logger: false });
+
+fastify.register(require("@fastify/static"), {
+  root: path.join(__dirname, "public"),
+  prefix: "/",
+});
+
+fastify.get("/", (request, reply) => {
+  reply.sendFile("index.html");
+});
+
+fastify.listen({ port: 3000, host: "0.0.0.0" });
